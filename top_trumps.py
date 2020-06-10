2db66eb568b5448b561efabb41af168f7dc29d73
LauraRamoskaite <ramoskaite.laura@gmail.com>
2020-06-10 20:23:58 +0100

    Opened new project with sample code

---
+++ top_trumps.py
@@ -0,0 +1,22 @@
+# Top Trumps project
+
+import random
+
+import requests
+
+def random_pokemon():
+    pokemon_number = random.randint(1, 151)
+    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+    response = requests.get(url)
+    pokemon = response.json()
+
+    return {
+        'name': pokemon['name'],
+        'id': pokemon['id'],
+        'height': pokemon['height'],
+        'weight': pokemon['weight'],
+        }
+
+random_pokemon()
+
+print(random_pokemon())
