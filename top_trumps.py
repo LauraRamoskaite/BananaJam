79349dbe8d8a891daefe06a77df981ddcc01edf1
LauraRamoskaite <ramoskaite.laura@gmail.com>
2020-06-13 20:35:55 +0100

    Retrieve pokemon function added

--- top_trumps.py
+++ top_trumps.py
@@ -4,19 +4,23 @@ import random
 
 import requests
 
+# Function to generate random pokemon ID
+def random_pokemon_ID():
+    random_integer = random.randint(1, 151)
+
+    return random_integer
+
+# Function to retrieve a pokemon
 def random_pokemon():
-    pokemon_number = random.randint(1, 151)
-    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon_number)
+    url = 'https://pokeapi.co/api/v2/pokemon/{}/'.format(random_pokemon_ID())
     response = requests.get(url)
     pokemon = response.json()
 
     return {
-        'name': pokemon['name'],
-        'id': pokemon['id'],
-        'height': pokemon['height'],
-        'weight': pokemon['weight'],
-        }
-
-random_pokemon()
+         'name': pokemon['name'],}
+#         'id': pokemon['id'],
+#         'height': pokemon['height'],
+#         'weight': pokemon['weight'],
+#         }
 
 print(random_pokemon())
