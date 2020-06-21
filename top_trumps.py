f4633a094a2d3e291819551405ca158808b99e90
siyanahh <66773870+siyanahh@users.noreply.github.com>
2020-06-21 20:00:49 +0100

    welcome message

--- top_trumps.py
+++ top_trumps.py
@@ -34,6 +34,7 @@ pokemon_card = random_pokemon()
 competing_cards = []
 
 # Function that selects 2 random cards; one for the player, one for the opponent
+
 def two_random_cards():
     for card in range(2):
         competing_cards.append(random_pokemon())
@@ -46,5 +47,26 @@ def start_the_game():
 # Calling the function to generate two random cards
 start_the_game()
 
+stat_choice = input('Which stat do you want to use? (id, height, weight) ')
 
+opponent_pokemon = random_pokemon()
+print('The opponent chose {}'.format(opponent_pokemon[id, height, weight])#here the opponent needs to choose a random stat
+
+my_stat = my_pokemon[stat_choice]
+opponent_stat = opponent_pokemon[stat_choice]
+
+if my_stat > opponent_stat:
+    print('You Win!')
+elif my_stat < opponent_stat:
+    print('You Lose!')
+else:
+    print('Draw!')
+
+#End of the game:
+play_again = input('Do you want to try again? yes/no ').title()
+    if play_again == 'yes'
+    elif play_again == 'no'
+    print ('Thanks for playing!')
+    else:
+    print('Invalid input, please try again!')
 #hello world
